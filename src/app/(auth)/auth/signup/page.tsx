80041aa624c64334686318629da92a0ed3776baf
alimarief <alimarief97@gmail.com>
2024-01-11 19:23:26 +0700

    Slicing signin & signup

--- src/app/(auth)/auth/signup/page.tsx
+++ src/app/(auth)/auth/signup/page.tsx
@@ -1,9 +1,103 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { signUpFormSchema } from "@/lib/form-schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Metadata } from "next";
+import Link from "next/link";
 import React, { FC } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 interface SignUpPageProps {}
 
 const SignUpPage: FC<SignUpPageProps> = ({}) => {
-  return <div>SignUpPage</div>;
+  const form = useForm<z.infer<typeof signUpFormSchema>>({
+    resolver: zodResolver(signUpFormSchema),
+  });
+
+  const onSubmit = async (val: z.infer<typeof signUpFormSchema>) => {};
+
+  return (
+    <div className="relative w-full h-screen">
+      <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+        <div className="mb-2 text-2xl font-semibold text-center">Sign Up</div>
+        <div className="text-sm text-gray-500">
+          Create your account to search products
+        </div>
+
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="mt-5 space-y-5"
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="Enter your name" {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="Enter your email" {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Enter your password"
+                      {...field}
+                    />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button className="w-full">Sign Up</Button>
+
+            <div className="text-sm text-center">
+              Already have an account{" "}
+              <Link href="/auth/signin" className="text-primary">
+                Sign In
+              </Link>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default SignUpPage;
