5e628be9f9d06caf8bb174cc99b26e2718c72410
alimarief <alimarief97@gmail.com>
2024-01-11 19:00:51 +0700

    Group project structure & added footer

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
           <div>
             <Header />
             {children}
+            <Footer />
           </div>
         </main>
       </body>
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react'
+
+interface FooterProps {
+  
+}
+
+const Footer: FC<FooterProps> = ({  }) => {
+  return (
+    <div className='bg-red-300 absolute bottom-0 w-full'>
+     Footer
+    </div>
+  )
+}
+
+export default Footer;
