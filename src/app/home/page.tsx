49ee65b70503043fb84920f017c76af8fb37a107
Nikki Rufiansya <nikkirufiansya88@gmail.com>
2024-01-11 19:49:29 +0700

    carousel

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,15 +1,60 @@
-import React, { FC } from 'react'
+"use client";
 
-interface HomeProps {
-  
-}
+import React from "react";
+import Slider from "react-slick";
+import Slide from "@/components/Slider/Slide";
 
-const Home: FC<HomeProps> = ({  }) => {
+const Home = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    pauseOnHover: false,
+  };
+
+  const slideData = [
+    {
+      id: 0,
+      img: "/banner-1.jpg",
+      title: "Trending Item",
+      mainTitle: "WOMEN'S LATEST FASHION SALE",
+      price: "$20",
+    },
+    {
+      id: 1,
+      img: "/banner-2.jpg",
+      title: "Trending Accessories",
+      mainTitle: "MODERN SUNGLASSES",
+      price: "$15",
+    },
+    {
+      id: 2,
+      img: "/banner-3.jpg",
+      title: "Sale Offer",
+      mainTitle: "NEW FASHION SUMMER SALE",
+      price: "$30",
+    },
+  ];
+
   return (
     <div>
-     Home
+      <div className="container mt-10">
+        <Slider {...settings}>
+          {slideData.map((item) => (
+            <Slide
+              key={item.id}
+              img={item.img}
+              title={item.title}
+              mainTitle={item.mainTitle}
+              price={item.price}
+            />
+          ))}
+        </Slider>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,11 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import HeaderTop from "@/components/Header/HeaderTop";
+import Footer from "@/components/Footer";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +25,9 @@ export default function RootLayout({
       <body className={inter.className}>
         <main>
           <div>
+            <HeaderTop />
             <Header />
+            
             {children}
           </div>
         </main>
