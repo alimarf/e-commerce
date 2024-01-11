4cf299ac25b64fc68182ad8b5bd505c3e595d890
alimarief <alimarief97@gmail.com>
2024-01-11 13:31:57 +0700

    first commit

---
+++ src/app/products/page.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react'
+
+interface ProductsProps {
+  
+}
+
+const Products: FC<ProductsProps> = ({  }) => {
+  return (
+    <div>
+     Products
+    </div>
+  )
+}
+
+export default Products;
---
+++ src/app/shopping-conditions/page.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react'
+
+interface ShoppingConditionsProps {
+  
+}
+
+const ShoppingConditions: FC<ShoppingConditionsProps> = ({  }) => {
+  return (
+    <div>
+     ShoppingConditions
+    </div>
+  )
+}
+
+export default ShoppingConditions;
