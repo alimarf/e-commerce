b51535d261bb0c0b02382b2c0a2820f31747bc08
alimarief <alimarief97@gmail.com>
2024-01-12 11:03:50 +0700

    Setup prisma & create api signup

---
+++ src/app/api/sign-up/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import { hashPassword } from "@/lib/utils";
+import prisma from "../../../../lib/prisma";
+
+export async function POST(request: Request) {
+  try {
+    const data = await request.json();
+
+    const hashedPassword = await hashPassword(data.password);
+
+    const result = await prisma.user.create({
+      data: {
+        name: data.name,
+        email: data.email,
+        password: hashedPassword,
+      },
+    });
+
+    return NextResponse.json({
+      status: 201,
+      message: "Signup success",
+      data: { name: result.name, email: result.email },
+    });
+  } catch (error) {
+    console.error("Error in signup:", error);
+
+    return NextResponse.json({
+      status: 500,
+      message: "Internal Server Error",
+    });
+  }
+}
