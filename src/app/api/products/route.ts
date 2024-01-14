00f8a782f7107c84617e0c4c3b7217388ea871ea
Nikki Rufiansya <nikkirufiansya88@gmail.com>
2024-01-14 12:34:47 +0700

    crud api product

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -9,7 +9,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 
 
-
+//Create Data Products
 export async function POST(request: Request) {
  
   try {
@@ -60,6 +60,138 @@ export async function POST(request: Request) {
     return NextResponse.json({
       status: 500,
       message: "Internal Server Error",
+    });
+  }
+}
+
+
+// READ Data Products
+export async function GET(request: NextApiRequest, response: NextApiResponse) {
+  try {
+    const products = await prisma.product.findMany();
+
+    return NextResponse.json({
+      status: 200,
+      message: "Products successfully retrieved",
+      data: products,
+    });
+  } catch (error) {
+    console.error("Failed to retrieve products:", error);
+
+    return NextResponse.json({
+      status: 500,
+      message: "Internal Server Error",
+    });
+  }
+}
+
+
+//UPDATE Data Product --> masih error
+export async function PUT(request: Request,req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const productId = req.query.id; // Assuming you have the product ID in the URL
+    const formData = await request.formData();
+
+    const product = await prisma.product.findUnique({
+      where: {
+        id: productId?.toString(),
+      },
+    });
+
+    if (!product) {
+      return NextResponse.json({
+        status: 404,
+        message: "Product not found",
+      });
+    }
+
+    const schema = zfd.formData({
+      name: zfd.text(),
+      description: zfd.text(),
+      price: zfd.numeric(z.number()),
+      rating: zfd.numeric(z.number().min(1).max(5)),
+      image: zfd.file()
+    });
+
+    const { name, description, price, rating, image } = schema.parse(await formData);
+
+    const updatedProduct = await prisma.product.update({
+      where: {
+        id: productId?.toString(),
+      },
+      data: {
+        name: name || product.name,
+        description: description || product.description,
+        price: price || product.price,
+        rating: rating || product.rating,
+        image: image ? path.join("public/images", image.name) : product.image,
+      },
+    });
+
+    // Update code for file if image is provided
+    if (image) {
+      const file: File | null = formData.get('image') as unknown as File;
+      const bytes = await file.arrayBuffer();
+      const buffer = Buffer.from(bytes);
+      const imagePath = path.join("public/images", image.name);
+      await writeFile(imagePath, buffer);
+    }
+
+    return NextResponse.json({
+      status: 200,
+      message: "Product successfully updated",
+      data: updatedProduct,
+    });
+  } catch (error) {
+    console.error("Product update failed:", error);
+
+    return NextResponse.json({
+      status: 500,
+      message: "Internal Server Error",
+    });
+  }
+}
+
+//DELETE Data Product --> Masih Error
+export async function DELETE(request: NextApiRequest, response: NextApiResponse) {
+  try {
+    const productId = request.query.id; // Assuming you have the product ID in the URL
+
+ 
+    
+    const product = await prisma.product.findUnique({
+      where: {
+        id: Number(productId).toString(),
+      },
+    });
+
+    if (!product) {
+      return NextResponse.json({
+        status: 404,
+        message: "Product not found",
+      });
+    }
+
+    // Delete the product
+    await prisma.product.delete({
+      where: {
+        id: Number(productId).toString(),
+      },
+    });
+
+    // Optionally, you can delete associated files (e.g., images) here
+
+    return NextResponse.json({
+      status: 200,
+      message: "Product successfully deleted",
+    });
+  } catch (error) {
+    console.error("Product deletion failed:", error);
+    
+    return NextResponse.json({
+      status: 500,
+      message: "Internal Server Error",
+     
     });
   }
 }
