d440ac8c34a4b52c9a548814ca140fb19f4c9433
alimarief <alimarief97@gmail.com>
2024-01-15 20:38:28 +0700

    Delete image and delete dummy home

--- src/app/(dashboard)/home/page.tsx
+++ src/app/(dashboard)/home/page.tsx
@@ -25,58 +25,6 @@ const Home = () => {
 
   console.log("data", productsData1);
 
-  const productsData = [
-    {
-      img: "/jacket-1.jpg",
-      title: "Jacket",
-      desc: "MEN Yarn Fleece Full-Zip Jacket",
-      rating: 4,
-      price: "900.000",
-    },
-    {
-      img: "/skirt-1.jpg",
-      title: "Skirt",
-      desc: "Black Floral Wrap Midi Skirt",
-      rating: 5,
-      price: "300.000",
-    },
-    {
-      img: "/party-wear-1.jpg",
-      title: "Party Wear",
-      desc: "Women's Party Wear Shoes",
-      rating: 3,
-      price: "250.000",
-    },
-    {
-      img: "/shirt-1.jpg",
-      title: "Shirt",
-      desc: "Pure Garment Dyed Cotton Shirt",
-      rating: 4,
-      price: "45.00",
-    },
-    {
-      img: "/sports-1.jpg",
-      title: "Sports",
-      desc: "Trekking & Running Shoes - Black",
-      rating: 3,
-      price: "58.000",
-    },
-    {
-      img: "/watch-1.jpg",
-      title: "Watches",
-      desc: "Smart Watches Vital Plus",
-      rating: 4,
-      price: "100.000",
-    },
-    {
-      img: "/watch-2.jpg",
-      title: "Watches",
-      desc: "Pocket Watch Leather Pouch",
-      rating: 4,
-      price: "120.000",
-    },
-  ];
-
   return (
     <div>
       <div className="container mt-5">
